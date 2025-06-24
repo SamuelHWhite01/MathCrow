fbe62b0ad8eecfb138c8513af0966c902a945fed
Samuel White <samuelhwhite01@gmail.com>
2025-06-24 15:34:54 -0700

    Added logic to confirm the user is logged in to display other pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import TimesTable from './screens/TimesTable';
 import LongMultiplication from './screens/LongMultiplication';
 import ActivitySelect from './screens/ActivitySelect';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 const App: React.FC = () => {
@@ -16,9 +17,11 @@ const App: React.FC = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<HomeScreen />} />
-                <Route path="/TimesTable" element={<TimesTable />} />
-                <Route path="/LongMultiplication" element={<LongMultiplication />} />
-                <Route path="/ActivitySelect" element={<ActivitySelect />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/TimesTable" element={<TimesTable />} />
+                    <Route path="/LongMultiplication" element={<LongMultiplication />} />
+                    <Route path="/ActivitySelect" element={<ActivitySelect />} />
+                </Route>
             </Routes>
         </Router>
         </UserDataProvider>
