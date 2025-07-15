070eeacd4cc911bc39d651209e94119d3c4f2325
Samuel White <samuelhwhite01@gmail.com>
2025-07-15 15:45:19 -0700

    WIP activity select screen

--- src/screens/ActivitySelect.tsx
+++ src/screens/ActivitySelect.tsx
@@ -1,9 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 const HomeScreen = () => {
+  const navigate = useNavigate()
   return (
-    <div className='leading-none m-auto flex flex-col w-fit h-fit gap-10'>
-      <Link to="/TimesTable">Go to Times Table</Link>
-      <Link to="/LongMultiplication">Go to Long Multiplication</Link>
+    <div className='flex flex-col m-auto items-center gap-5'>
+      <div className='text-[10vh]'>
+        Select an activity
+      </div>
+      <div className=' flex flex-row m-auto items-center gap-5'>
+        <div className='flex flex-col items-center text-[3vh] font-bold'>
+          <button className=' h-[20vh] w-[20vh] 
+          bg-[url("/icon1.png")]
+          bg-cover
+          rounded 
+          hover:cursor-pointer hover:scale-110 transform transition-transform duration-150'
+          onClick={(e) => navigate('/TimesTable')}>        
+          </button>
+          Times Table
+        </div>
+        <div className='h-[20vh] w-[20vh] bg-blue-300 rounded'>
+
+        </div>
+      </div>
+      <div className='leading-none m-auto flex flex-col w-fit h-fit gap-10'>
+        <Link to="/TimesTable">Go to Times Table</Link>
+        <Link to="/LongMultiplication">Go to Long Multiplication</Link>
+      </div>
     </div>
   );
 };
