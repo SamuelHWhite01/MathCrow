bbffa5d5f8895c4b5a8f593b2bf5de5b3ad49bc4
Samuel White <samuelhwhite01@gmail.com>
2026-02-23 08:21:43 -0800

    Removed test stripe button

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import SetupClassroom from '@/components/homescreen/SetupClassroom';
 import PlayButton from '../components/homescreen/PlayButton';
 import JoinByCode from '@/components/homescreen/JoinByCode';
-import StripeButton from '@/components/homescreen/StripeButton';
 const HomeScreen = () => {
   return (
     <div className='leading-none m-auto flex flex-col w-fit h-fit gap-10'>
@@ -9,7 +8,6 @@ const HomeScreen = () => {
       <PlayButton/>
       <SetupClassroom/>
       <JoinByCode/>
-      <StripeButton/>
     </div>
   );
 };
